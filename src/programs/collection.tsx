dc6480ab48581dac9b73106a40e091463dd74d41
JJ <44841139+Auer111@users.noreply.github.com>
2024-02-22 14:06:05 -0600

    filter deleted programs

--- src/programs/collection.tsx
+++ src/programs/collection.tsx
@@ -42,33 +42,35 @@ export const ProgramCollection: React.FC<ProgramCollectionProps> = ({
       <MirrorText text="Programs" />
       <Container sx={{ pt: 2, pb: 2 }}>
         <Grid container spacing={2}>
-          {data!.map((camp, index) => (
-            <Grid item key={index} xs={12} sm={6} md={4}>
-              <Card
-                onClick={() => redirect(camp.id)}
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                }}
-              >
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={camp?.image?.src}
-                  alt={camp?.image?.name}
-                  sx={{ width: "8em", height: "8em" }}
-                />
-                <CardContent sx={{ flexGrow: 1 }}>
-                  <Typography gutterBottom variant="h6" component="div">
-                    {camp.name}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {camp.description}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
+          {data!
+            ?.filter((camp) => camp.name)
+            .map((camp, index) => (
+              <Grid item key={index} xs={12} sm={6} md={4}>
+                <Card
+                  onClick={() => redirect(camp.id)}
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={camp?.image?.src}
+                    alt={camp?.image?.name}
+                    sx={{ width: "8em", height: "8em" }}
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h6" component="div">
+                      {camp.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {camp.description}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
         </Grid>
       </Container>
     </>
